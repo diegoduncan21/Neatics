69b0494059380af5fdb51b1b47b27acf1e66a9aa
diegoduncan21 <diegoduncan21@gmail.com>
2013-08-26 10:09:36 -0300

    app portal agregado

--- Neatics/settings.py
+++ Neatics/settings.py
@@ -3,6 +3,9 @@
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+import os
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__name__))
+
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
---
+++ portal/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('portal.views',
+    
+    # url(r'^$', 'home', name='home'),
+    
+)
